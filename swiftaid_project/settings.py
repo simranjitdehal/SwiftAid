3e74b55581b8ecab2b642fe10cd26b198bb482e1
Simranjit Singh Dehal <simranjitsinghdehal@gmail.com>
2025-12-31 21:15:10 +0530

    storages in settings

--- swiftaid_project/settings.py
+++ swiftaid_project/settings.py
@@ -132,3 +132,12 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
